9468b4af398b8cb8f031a1b87b8410672b54562e
Johannes Breidenbach <johannes.breidenbach@gmx.de>
2021-11-02 13:36:44 +0100

    correction and frendlier to use

--- rename.py
+++ rename.py
@@ -1,9 +1,9 @@
 # -*- coding: iso-8859-1 -*-
 import os
-from os import getcwd, chdir, rename
 import csv
 
 #settings
+extension = 'pdf'                                                   #adjust if an other file
 correction = 'Korrektur'
 duty = 'Abgabe_01'                                                  #adjust
 path = 'C:/Users/user/test_path/'                                   #adjust Care: Windows replace '\' to '/' or use the r 
@@ -13,13 +13,13 @@ os.chdir(path)                                                      #other oppor
 student_name = []
 for pdf in os.listdir('.'):
         name, rest = pdf.split('_',1)                               #seperate name e.g. max mustermann_112_homework1_group1.pdf
-        unimportant, extension = rest.split('.')                    #get the extension e.g. pdf
         new_name = f'{correction}_{duty}_{name}.{extension}'        #create the new name of the document or file
         student_name.append(name)                                   #save the names in an array
         os.rename(pdf,new_name)                                     #rename the files or documents
 
 #save names in csv
-with open('Korrektur_Abgabe_01.csv', 'w', newline='') as csvfile:    #adjust 
+name_csv = f'{correction}_{duty}.csv'
+with open(name_csv, 'w', newline='') as csvfile:
     
     fieldnames = ['Studierende']
     thewriter = csv.DictWriter(csvfile, fieldnames=fieldnames)
