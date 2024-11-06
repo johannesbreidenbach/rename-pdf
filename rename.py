1928d9bdc26f57374390ba9a095298ef7e3270b5
Johannes Breidenbach <92515741+johannesbreidenbach@users.noreply.github.com>
2024-11-06 09:10:51 +0100

    Update rename.py
    -Add better path handling for all operating systems.
    -Option to chose between csv and excel file to save name list.

--- rename.py
+++ rename.py
@@ -1,24 +1,50 @@
 # -*- coding: iso-8859-1 -*-
+"""
+Author: Breidenbach, Johannes
+Date: 2021-10-26
+Description: Rename pdf files and save the new names in csv file
+"""
+"""
+Required packages:
+-os
+-csv
+-pandas
+-openpyxl
+install with pip or conda
+"""
 import os
 import csv
+import pandas as pd
 
-#settings
-extension = 'pdf'                                                                       # adjust if an other file
-correction = 'correction'
-submission = 'submission_01'                                                            # adjust
-path = 'C:/Users/user/test_path/'                                                       # adjust Care: Windows replace '\' to '/' or use the r 
-os.chdir(path)                                                                          # other opportunity: os.chdir(r'C:\Users\user\test_path')
+"""settings"""
+save_csv = False                                                                        # adjust which file is requierd by saving names list
+save_excel = True
+extension = 'pdf'                                                                       # adjust if an other file is in folder
+correction = 'submission_01'                                                            # adapt to use case
+number = '01'                                                                           # adapt to use case
+submission = 'Abgabe_' + number                                                         # adapt to use case
+path = os.path.join('.', 'your', 'path')                                                # Adapt this to your folder structure
+os.chdir(path)                                                                          # other opportunity: os.chdir(r'C:\Users\user\your\path')
 
-#rename files
-student_name = [['Studentys']]                                                          # column heading
+"""rename files"""
+student_name = [['Studentys']]
+student_name_excel = []
 for pdf in os.listdir('.'):
         name, rest = pdf.split('_',1)                                                   # seperate name e.g. max mustermann_112_homework1_group1.pdf
-        new_name = f'{correction}_{submission}_{name.replace(' ','_')}.{extension}'     # create the new name of the document or file and change space to _ (max_mustermann)
-        student_name.append([name])                                                     # save the names in an array
+        new_name = f'{correction}_{submission}_{name.replace(' ','_')}.{extension}'     # create the new name of the document or file
+        student_name.append([name])                                                     # save the names in a list
+        student_name_excel.append([name])                                               # save the names in a list
         os.rename(pdf,new_name)                                                         # rename the files or documents
 
-#save names in csv
-name_csv = f'{correction}_{submission}.csv'
-with open(name_csv, 'w', newline='', encoding='utf-8-sig') as csvfile:                  # encoding='utf-8-sig' for better view in excel
-    thewriter = csv.writer(csvfile, dialect='excel')
-    thewriter.writerows(student_name)
+"""save names in csv"""
+if save_csv:
+        name_csv = f'{correction}_{submission}.csv'
+        with open(name_csv, 'w', newline='', encoding='utf-8-sig') as csvfile:          # encoding='utf-8-sig' for better view in excel
+        thewriter = csv.writer(csvfile, dialect='excel')
+        thewriter.writerows(student_name)
+
+"""save names in excel"""
+if save_excel:
+        name_excel = f'{correction}_{submission}.xlsx'
+        df = pd.DataFrame(student_name_excel, columns=['Studentys'])
+        df.to_excel(name_excel, index=False)
