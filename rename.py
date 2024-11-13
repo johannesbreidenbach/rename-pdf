6e8f917e59e458fd7134c9019a3596c17b90e01a
Johannes Breidenbach <92515741+johannesbreidenbach@users.noreply.github.com>
2024-11-13 08:33:17 +0100

    Bug fixes

--- rename.py
+++ rename.py
@@ -24,12 +24,16 @@ correction = 'submission_01'                                                    
 number = '01'                                                                           # adapt to use case
 submission = 'Abgabe_' + number                                                         # adapt to use case
 path = os.path.join('.', 'your', 'path')                                                # Adapt this to your folder structure
-os.chdir(path)                                                                          # other opportunity: os.chdir(r'C:\Users\user\your\path')
+os.chdir(path)                                                                          # change the working directory, other opportunity: os.chdir(r'C:\Users\user\your\path')
 
 """rename files"""
+list_files = os.listdir('.')                                                            # crate list of all files in directory
+list_pdfs = [file for file in list_files if file.endswith('.pdf')]                      # write all pdf documents in a new list
+
 student_name = [['Studentys']]
 student_name_excel = []
-for pdf in os.listdir('.'):
+
+for pdf in list_pdfs:
         name, rest = pdf.split('_',1)                                                   # seperate name e.g. max mustermann_112_homework1_group1.pdf
         new_name = f'{correction}_{submission}_{name.replace(' ','_')}.{extension}'     # create the new name of the document or file
         student_name.append([name])                                                     # save the names in a list
@@ -40,8 +44,8 @@ for pdf in os.listdir('.'):
 if save_csv:
         name_csv = f'{correction}_{submission}.csv'
         with open(name_csv, 'w', newline='', encoding='utf-8-sig') as csvfile:          # encoding='utf-8-sig' for better view in excel
-        thewriter = csv.writer(csvfile, dialect='excel')
-        thewriter.writerows(student_name)
+                thewriter = csv.writer(csvfile, dialect='excel')
+                thewriter.writerows(student_name)
 
 """save names in excel"""
 if save_excel:
